7b4d8fb85f9fc235133c24d4da67bb3d25096c7b
Reinoud van Zoelen <reinoud95@hotmail.com>
2017-05-09 09:30:14 +0200

    changed bank to auction and added the databasecleaner

---
+++ src/test/java/util/DatabaseCleaner.java
@@ -0,0 +1,40 @@
+package util;
+
+import auction.Models.Bid;
+import auction.Models.Category;
+import auction.Models.Item;
+import auction.Models.User;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+public class DatabaseCleaner {
+
+    private static final Class<?>[] ENTITY_TYPES = {
+        Bid.class, Category.class, Item.class, User.class
+    };
+    private final EntityManager em;
+
+    public DatabaseCleaner(EntityManager entityManager) {
+        em = entityManager;
+    }
+
+    public void clean() throws SQLException {
+        em.getTransaction().begin();
+
+        for (Class<?> entityType : ENTITY_TYPES) {
+            deleteEntities(entityType);
+        }
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    private void deleteEntities(Class<?> entityType) {
+        em.createQuery("delete from " + getEntityName(entityType)).executeUpdate();
+    }
+
+    protected String getEntityName(Class<?> clazz) {
+        EntityType et = em.getMetamodel().entity(clazz);
+        return et.getName();
+    }
+}
